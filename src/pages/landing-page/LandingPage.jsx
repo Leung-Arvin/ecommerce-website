361df1d71553502abdfe6b61e5d06cabf1612d7b
Arvin-Leung <leungarvin@gmail.com>
2025-06-28 21:24:53 -0400

    added matcha products with images and created main banner

---
+++ src/pages/landing-page/LandingPage.jsx
@@ -0,0 +1,10 @@
+import "./LandingPage.css"
+import MainBanner from "../../components/main-banner/MainBanner"
+
+export default function LandingPage() {
+  return(
+    <div className="landing-container">
+      <MainBanner/>
+    </div>
+  )
+}
